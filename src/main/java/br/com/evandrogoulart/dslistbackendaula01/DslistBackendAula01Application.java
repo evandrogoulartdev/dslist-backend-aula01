56080d7a7feafc42599cba782509926c617b13dd
evandrodev <evandrodev@bestdevs.com>
2023-05-19 00:27:16 -0300

    Project setup

---
+++ src/main/java/br/com/evandrogoulart/dslistbackendaula01/DslistBackendAula01Application.java
@@ -0,0 +1,13 @@
+package br.com.evandrogoulart.dslistbackendaula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackendAula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackendAula01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/evandrogoulart/dslistbackendaula01/DslistBackendAula01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evandrogoulart.dslistbackendaula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendAula01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
